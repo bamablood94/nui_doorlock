e41da652bffb2a96ae43dd008741ccf4c51e14f1
Bama94 <tegober94@hotmail.com>
2022-02-15 22:34:01 -0600

    added doors

--- configs/lsc.lua
+++ configs/lsc.lua
@@ -1,134 +1,188 @@
-
-
--- lsc garage doors created by Bama94
-Config.DoorList['lsc garage doors'] = {
-	locked = true,
-	lockpick = false,
-	slides = true,
-	garage = true,
-	maxDistance = 6.0,
-	audioRemote = false,
-	fixText = false,
-	objCoords = vec3(-321.673431, -141.179764, 39.625530),
-	objHash = 267415464,
-	authorizedJobs = { ['police']=0, ['mechanic']=0 },
-	objHeading = 249.99995422363,
+-- garagefront1 created by Bama94
+Config.DoorList['garagefront1'] = {
+    objCoords = vec3(-349.495453, -117.288200, 40.031055),
+    audioRemote = false,
+    authorizedJobs = { ['mechanic']=0 },
+    objHash = -550347177,
+    fixText = false,
+    garage = true,
+    maxDistance = 6.0,
+    locked = true,
+    lockpick = false,
+    objHeading = 249.99996948242,
+    slides = true,
     --oldMethod = true,
     --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
     --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-    --autoLock = 1000
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
 }
 
--- lsclockeroom created by Bama94
-Config.DoorList['lsclockeroom'] = {
-	objHash = -1563799200,
-	audioRemote = false,
-	maxDistance = 2.0,
-	garage = false,
-	slides = false,
-	fixText = false,
-	objCoords = vec3(-320.885773, -138.852356, 39.122360),
-	lockpick = false,
-	locked = true,
-	objHeading = 248.94854736328,
-	authorizedJobs = { ['police']=0, ['mechanic']=0 },
+-- garagefront2 created by Bama94
+Config.DoorList['garagefront2'] = {
+    objCoords = vec3(-356.090485, -134.771423, 40.012955),
+    audioRemote = false,
+    authorizedJobs = { ['mechanic']=0 },
+    objHash = -550347177,
+    fixText = false,
+    garage = true,
+    maxDistance = 6.0,
+    locked = true,
+    lockpick = false,
+    objHeading = 249.99995422363,
+    slides = true,
     --oldMethod = true,
     --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
     --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-    --autoLock = 1000
-}
-
--- lscoffice created by Bama94
-Config.DoorList['lscoffice'] = {
-	objHash = -1259801187,
-	audioRemote = false,
-	maxDistance = 2.0,
-	garage = false,
-	slides = false,
-	fixText = false,
-	objCoords = vec3(-312.163116, -115.242271, 39.112598),
-	lockpick = false,
-	locked = true,
-	objHeading = 68.999946594238,
-	authorizedJobs = { ['police']=0, ['mechanic']=0 },
-    --oldMethod = true,
-    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
-    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-    --autoLock = 1000
-}
-
--- lscfrontoffice created by Bama94
-Config.DoorList['lscfrontoffice'] = {
-	objHash = -1563799200,
-	audioRemote = false,
-	maxDistance = 2.0,
-	garage = false,
-	slides = false,
-	fixText = false,
-	objCoords = vec3(-346.038147, -123.546654, 39.050488),
-	lockpick = false,
-	locked = true,
-	objHeading = 249.00061035156,
-	authorizedJobs = { ['police']=0, ['mechanic']=0 },
-    --oldMethod = true,
-    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
-    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-    --autoLock = 1000
-}
-
--- lscfrontoffice created by Bama94
-Config.DoorList['lscfrontoffice'] = {
-	objHash = -1259801187,
-	audioRemote = false,
-	maxDistance = 2.0,
-	garage = false,
-	slides = false,
-	fixText = false,
-	objCoords = vec3(-347.604492, -132.628174, 39.067551),
-	lockpick = false,
-	locked = true,
-	objHeading = 68.999946594238,
-	authorizedJobs = { ['police']=0, ['mechanic']=0 },
-    --oldMethod = true,
-    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
-    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-    --autoLock = 1000
-}
-
--- frontofficeside1 created by Bama94
-Config.DoorList['frontofficeside1'] = {
-	locked = true,
-	lockpick = false,
-	garage = false,
-	authorizedJobs = { ['police']=0, ['mechanic']=0 },
-	maxDistance = 2.0,
-	objHash = -1563799200,
-	objHeading = 249.00061035156,
-	audioRemote = false,
-	fixText = false,
-	slides = false,
-	objCoords = vec3(-346.038147, -123.546654, 39.050488),
-    --oldMethod = true,
-    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
-    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-    --autoLock = 1000
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
 }
 
 -- paintbooth created by Bama94
 Config.DoorList['paintbooth'] = {
-	audioRemote = false,
-	objHash = 1544229216,
-	slides = true,
-	lockpick = false,
-	fixText = false,
-	authorizedJobs = { ['police']=0, ['mechanic']=0 },
-	objHeading = 249.99995422363,
-	objCoords = vec3(-330.432709, -143.392944, 39.302750),
-	maxDistance = 6.0,
-	garage = true,
-	locked = true,
+    objCoords = vec3(-330.432709, -143.392944, 39.302750),
+    slides = true,
+    garage = true,
+    lockpick = false,
+    objHeading = 249.99995422363,
+    audioRemote = false,
+    authorizedJobs = { ['mechanic']=0 },
+    fixText = false,
+    locked = true,
+    maxDistance = 6.0,
+    objHash = 1544229216,
     --oldMethod = true,
     --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
     --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
-    --autoLock = 1000
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- backside1 created by Bama94
+Config.DoorList['backside1'] = {
+    objCoords = vec3(-320.885773, -138.852356, 39.122360),
+    slides = false,
+    garage = false,
+    lockpick = false,
+    objHeading = 249.00061035156,
+    audioRemote = false,
+    authorizedJobs = { ['mechanic']=0 },
+    fixText = false,
+    locked = true,
+    maxDistance = 2.0,
+    objHash = -1563799200,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- parkinggarage created by Bama94
+Config.DoorList['parkinggarage'] = {
+    objCoords = vec3(-321.673431, -141.179764, 39.625530),
+    slides = true,
+    garage = true,
+    lockpick = false,
+    objHeading = 249.99995422363,
+    audioRemote = false,
+    authorizedJobs = { ['mechanic']=0 },
+    fixText = false,
+    locked = true,
+    maxDistance = 3.0,
+    objHash = 267415464,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- backside2 created by Bama94
+Config.DoorList['backside2'] = {
+    objCoords = vec3(-312.163116, -115.242271, 39.112598),
+    slides = false,
+    garage = false,
+    lockpick = false,
+    objHeading = 68.999946594238,
+    audioRemote = false,
+    authorizedJobs = { ['mechanic']=0 },
+    fixText = false,
+    locked = true,
+    maxDistance = 2.0,
+    objHash = -1259801187,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- frontofficeside1 created by Bama94
+Config.DoorList['frontofficeside1'] = {
+    objCoords = vec3(-346.038147, -123.546654, 39.050488),
+    slides = false,
+    garage = false,
+    lockpick = false,
+    objHeading = 249.00061035156,
+    audioRemote = false,
+    authorizedJobs = { ['mechanic']=0 },
+    fixText = false,
+    locked = true,
+    maxDistance = 2.0,
+    objHash = -1563799200,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- frontofficeside1 created by Bama94
+Config.DoorList['frontofficeside2'] = {
+    objCoords = vec3(-347.604492, -132.628174, 39.067551),
+    slides = false,
+    garage = false,
+    lockpick = false,
+    objHeading = 69.015975952148,
+    audioRemote = false,
+    authorizedJobs = { ['mechanic']=0 },
+    fixText = false,
+    locked = true,
+    maxDistance = 2.0,
+    objHash = -1259801187,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- lobbydoor created by Bama94
+Config.DoorList['lobbydoor'] = {
+    objCoords = vec3(-353.861328, -127.761383, 39.532822),
+    slides = false,
+    garage = false,
+    lockpick = false,
+    objHeading = 68.999946594238,
+    audioRemote = false,
+    authorizedJobs = { ['mechanic']=0 },
+    fixText = false,
+    locked = false,
+    maxDistance = 2.0,
+    objHash = -1259801187,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
 }
