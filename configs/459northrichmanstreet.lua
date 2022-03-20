a52e8fdbef26b33509b62af33372f220c3db8352
Bama94 <tegober94@hotmail.com>
2022-03-20 13:36:29 -0500

    added doorlock

---
+++ configs/459northrichmanstreet.lua
@@ -0,0 +1,147 @@
+
+
+-- garagedoor created by Bama94
+Config.DoorList['garagedoor'] = {
+    locked = true,
+    authorizedCIDs = { ['QIQ84498']=true },
+    objHeading = 303.88256835938,
+    slides = true,
+    garage = true,
+    objHash = 634017584,
+    lockpick = false,
+    objCoords = vec3(-1744.944092, 368.653229, 90.061592),
+    maxDistance = 6.0,
+    fixText = false,
+    audioRemote = false,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- garagesidedoor created by Bama94
+Config.DoorList['garagesidedoor'] = {
+    locked = true,
+    authorizedCIDs = { ['QIQ84498']=true },
+    objHeading = 304.17541503906,
+    slides = true,
+    garage = true,
+    objHash = -826011544,
+    lockpick = false,
+    objCoords = vec3(-1740.882080, 364.889404, 89.089592),
+    maxDistance = 6.0,
+    fixText = false,
+    audioRemote = false,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- frontdoor created by Bama94
+Config.DoorList['frontdoor'] = {
+    locked = true,
+    authorizedCIDs = { ['QIQ84498']=true },
+    objHeading = 27.866580963135,
+    slides = false,
+    garage = false,
+    objHash = -826011544,
+    lockpick = false,
+    objCoords = vec3(-1732.161987, 379.020752, 90.086578),
+    maxDistance = 2.0,
+    fixText = false,
+    audioRemote = false,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- officedoor created by Bama94
+Config.DoorList['officedoor'] = {
+    locked = true,
+    authorizedCIDs = { ['QIQ84498']=true },
+    objHeading = 7.3669142723083,
+    slides = false,
+    garage = false,
+    objHash = 1432392886,
+    lockpick = false,
+    objCoords = vec3(-1722.569824, 375.496979, 90.045876),
+    maxDistance = 2.0,
+    fixText = false,
+    audioRemote = false,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- bedroomdoor created by Bama94
+Config.DoorList['bedroomdoor'] = {
+    locked = true,
+    authorizedCIDs = { ['QIQ84498']=true },
+    objHeading = 297.86657714844,
+    slides = false,
+    garage = false,
+    objHash = 1432392886,
+    lockpick = false,
+    objCoords = vec3(-1717.780396, 373.945770, 90.045876),
+    maxDistance = 2.0,
+    fixText = false,
+    audioRemote = false,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- bathroomdoor created by Bama94
+Config.DoorList['bathroomdoor'] = {
+    locked = true,
+    authorizedCIDs = { ['QIQ84498']=true },
+    objHeading = 132.56687927246,
+    slides = false,
+    garage = false,
+    objHash = 1432392886,
+    lockpick = false,
+    objCoords = vec3(-1738.594482, 364.394348, 89.747879),
+    maxDistance = 2.0,
+    fixText = false,
+    audioRemote = false,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- backdoor created by Bama94
+Config.DoorList['backdoor'] = {
+    locked = true,
+    authorizedCIDs = { ['QIQ84498']=true },
+    lockpick = false,
+    doors = {
+        {objHash = 1664299099, objHeading = 41.771041870117, objCoords = vec3(-1724.774292, 358.533844, 90.083580)},
+        {objHash = 1664299099, objHeading = 221.47198486328, objCoords = vec3(-1722.409790, 360.635895, 90.083580)}
+    },
+    slides = false,
+    maxDistance = 2.5,
+    audioRemote = false,
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
